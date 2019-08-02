bcdece91fcb67b54cc3058c740688a562c3bc3e6
Andrew <ans222@pitt.edu>
2019-08-02 15:25:31 -0400

    Almost finished with documentation

---
+++ Archived Code/rmNoiseFloor.m
@@ -0,0 +1,7 @@
+function [floorx,floory] = getNoiseFloor(pos,neg,N)
+%rmNoiseFloor Sre
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- rmNoiseFloor.m
+++
@@ -1,7 +0,0 @@
-function [floorx,floory] = getNoiseFloor(pos,neg,N)
-%rmNoiseFloor Sre
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
